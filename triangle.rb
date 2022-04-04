24e39657cd7896716c274dbec10a12308cf19d64
thaong91 <tnguye13@chicagobooth.edu>
2022-04-04 04:09:29 +0000

    finish about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,18 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && b == c
-    return :equilateral
-  elsif (a == b && a != c) || (a != b && b == c) || (a == c && a != b)
-    return :isosceles
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError, "A triangle side cannot be 0."
+  elsif (a + b <= c) || (a + c <= b) || (b + c <= a)
+    raise TriangleError, "Sum of two triangle sides must be larger than the third side."
   else
-    return :scalene
+    if a == b && b == c
+      return :equilateral
+    elsif (a == b && a != c) || (a != b && b == c) || (a == c && a != b)
+      return :isosceles
+    else
+      return :scalene
+    end
   end
 end
 
